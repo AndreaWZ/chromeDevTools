b8b0821b9cdc1207d053c116805b9e9688363ad5
Andrea Wu <lixianhyun@gmail.com>
2020-11-19 23:12:51 -0800

    types of 'console'

--- main.js
+++ main.js
@@ -9,3 +9,27 @@ const dogs = [{ name: 'Snickers', age: 2 }, { name: 'hugo', age: 8 }];
       p.style.color = '#BADA55';
       p.style.fontSize = '50px';
     }
+
+// Regular
+
+// Interpolated
+
+// Styled
+
+// warning!
+
+// Error :|
+
+// Info
+
+// Testing
+
+// clearing
+
+// Viewing DOM Elements
+
+// Grouping together
+
+// counting
+
+// timing
